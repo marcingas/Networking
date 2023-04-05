71a15d291031f15b0abf02b4be2c3524ec8398e9
Marcin <gasiorek.mail@gmail.com>
2023-04-05 08:53:33 +0200

    URI parts

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            URI uriu = new URI("db://username:password@myserver.com:5000/catalogue/phones?os=android#samsung");
+            System.out.println("Scheme= " + uriu.getScheme());
+            System.out.println("Scheme-specific part= " + uriu.getSchemeSpecificPart());
+            System.out.println("Authority= " + uriu.getAuthority());
+            System.out.println("User-info= " + uriu.getUserInfo());
+            System.out.println("host= " + uriu.getHost());
+            System.out.println("port= " + uriu.getPort());
+            System.out.println("path= " + uriu.getPath());
+            System.out.println("query= " + uriu.getQuery());
+            System.out.println("fragment= " + uriu.getFragment());
+        }catch (URISyntaxException e){
+            System.out.println("URI Bad Syntax: " + e.getMessage());
+
+        }
+
     }
 }
