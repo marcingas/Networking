6b9e18b4772e6cd0dec06a045921a891a3caf98d
Marcin <gasiorek.mail@gmail.com>
2023-04-05 09:07:04 +0200

    convert to URL, relative URI and base URI

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,24 @@ import java.net.URL;
 public class Main {
     public static void main(String[] args) {
         try {
-            URI uriu = new URI("http://username:password@myserver.com:5000/catalogue/phones?os=android#samsung");
-            URL url = uriu.toURL();
+            URI uri = new URI("http://username:password@myserver.com:5000/catalogue/phones?os=android#samsung");
+            URI baseUri = new URI("http://username:password@myserver.com:5000");
+            URI relativeUri = new URI("/catalogue/phones?os=android#samsung");
+            URI resolvedUri = baseUri.resolve(relativeUri);
+            URL url2 = resolvedUri.toURL();
+            System.out.println("URL (From resolved) =" + url2);
+            URL url = uri.toURL();
             System.out.println("URL= " + url);
 
-//            System.out.println("Scheme= " + uriu.getScheme());
-//            System.out.println("Scheme-specific part= " + uriu.getSchemeSpecificPart());
-//            System.out.println("Authority= " + uriu.getAuthority());
-//            System.out.println("User-info= " + uriu.getUserInfo());
-//            System.out.println("host= " + uriu.getHost());
-//            System.out.println("port= " + uriu.getPort());
-//            System.out.println("path= " + uriu.getPath());
-//            System.out.println("query= " + uriu.getQuery());
-//            System.out.println("fragment= " + uriu.getFragment());
+//            System.out.println("Scheme= " + uri.getScheme());
+//            System.out.println("Scheme-specific part= " + uri.getSchemeSpecificPart());
+//            System.out.println("Authority= " + uri.getAuthority());
+//            System.out.println("User-info= " + uri.getUserInfo());
+//            System.out.println("host= " + uri.getHost());
+//            System.out.println("port= " + uri.getPort());
+//            System.out.println("path= " + uri.getPath());
+//            System.out.println("query= " + uri.getQuery());
+//            System.out.println("fragment= " + uri.getFragment());
         }catch (URISyntaxException e){
             System.out.println("URI Bad Syntax: " + e.getMessage());
         }catch(MalformedURLException e){
